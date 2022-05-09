0db690dea49fde14dd648d33022f87bfbc4ad178
Jo Ann Sutojo <joannsutojo@gmail.com>
2022-05-09 09:15:50 +0200

    added distance travelled

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,55 @@
-import React, { useReducer } from 'react';
+import React, { useReducer, useEffect } from 'react';
 import '../components/App.css'
 
 const initialState = {
     engine: 'OFF',
     gear: 0,
     speed: 0,
-    distanceTravelled: 0
+    distance: 0
 };
 
 const reducer = function (state, action) {
     switch(action.type) {
-        case 'power': {
-            return  {
-                ...state, 
-                engine: state.engine === 'OFF' ? 'ON' : 'OFF', 
-                gear: state.engine === 'ON' ? 0 : state.gear,
-                speed: state.engine === 'ON' ? 0 : state.speed,
-             }
+        case 'start': {
+            //  start engine with 50% chance to fail
+            if (Math.random() > 0.5) {
+               return {
+                   ...state,
+                   engine: 'ON'
+               }
+            }
+            return state;
+        }
+        case 'stop': {
+            return {
+                ...state,
+                engine: 'OFF',
+                gear: 0
+            }
+        }
+
+        // start/stop button (start engine with 50% chance to fail)
+        // case 'toggle': {
+        //     if (state.engine === 'OFF' && Math.random() > 0.5) {
+        //         return {
+        //             ...state,
+        //             engine: 'ON'
+        //         }
+        //     } else if (state.engine === 'OFF') {
+        //         return state;
+        //     } else {
+        //         return {
+        //             ...state,
+        //             engine: 'OFF'
+        //         }
+        //     }
+        // }
+
+        case 'move': {
+            return {
+                ...state,
+                distance: state.distance + state.speed
+            }
         }
         case 'gearUp': 
             if (state.engine === 'ON') {
@@ -28,7 +61,7 @@ const reducer = function (state, action) {
         } else return state;
         case 'accelerate': 
              if (state.engine === 'ON' && state.gear !== 0) {
-            return {...state, speed: state.speed + (10 * state.gear)}
+            return {...state, speed: state.speed + Math.abs(10 * state.gear)}
         } else return state;
         case 'decelerate': 
              if (state.engine === 'ON' && state.gear !== 0) {
@@ -43,17 +76,31 @@ const reducer = function (state, action) {
 function App() {
     const [state, dispatch] = useReducer(reducer, initialState);
 
+    // Bonus
+    // The setInterval() method calls a function at specified intervals (in milliseconds).
+    // 1 second = 1000 milliseconds.
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            dispatch({
+                type: 'move'
+            })
+        }, 1000)
+        return () => { clearInterval(interval)};
+    }, [])
+
     return (
         <div className='container'>
-            <h1>Boat UI</h1>
+            <h1>Boating with useReducer</h1>
             <div>
                 <h2>Engine {state.engine}</h2>
                 <h2>Gear: {state.gear}</h2>
                 <h2>Speed: {state.speed} Km/h</h2>
-                <h2>Distance travelled: {state.distanceTravelled} Km</h2>
+                <h2>Distance travelled: {state.distance} Km</h2>
             </div>
             <div>
-                <button onClick={() => dispatch({type: 'power'})}>Start/Stop</button>
+                <button onClick={() => dispatch({type: 'start'})}>Start Engine</button>
+                <button onClick={() => dispatch({type: 'stop'})}>Stop Engine</button>
                 <button onClick={() => dispatch({type: 'gearUp'})}>Gear Up</button>
                 <button onClick={() => dispatch({type: 'gearDown'})}>Gear Down</button>
                 <button onClick={() => dispatch({type: 'accelerate'})}>Accelerate</button>
